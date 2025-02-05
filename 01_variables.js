68146a7cca2bab47b06e33b2ef35de4494ecf153
Prem Shandilya <you@example.com>
2025-02-06 02:54:57 +0530

    added 01_variables.js

---
+++ 01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 1234;
+var accountEmail = "Premshandilya@gmail.com";
+let accountPassword = "12346";
+accountName = "Prem Shandilya";
+
+//accountId = "3466"; //This is not allowed as accountId is declared as const
+accountEmail = "Prateek@gmail.com";
+accountPassword = "897986";
+accountName = "Prateek Shandilya";
+
+console.table([accountId, accountEmail, accountPassword, accountName]);
+console.log(accountPassword);

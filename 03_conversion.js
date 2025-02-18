ca709019adacce8adfe92587697c21a231c82f3a
Prem Shandilya <you@example.com>
2025-02-19 03:35:48 +0530

    added new code

--- 03_conversion.js
+++ 03_conversion.js
@@ -2,3 +2,25 @@ let score = 1;
 valueInNumber = Number(score);
 
 console.log(typeof score);
+
+// *******************Operations*************
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "Hello";
+let str2 = " Prem";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + "2");
+console.log(1 + 2 + "2");
